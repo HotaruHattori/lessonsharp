18d5d67551ae7307c2b1bdfe98d53441532f0b65
HotaruHattori <75927087+HotaruHattori@users.noreply.github.com>
2021-04-04 15:29:39 +0600

    lol

---
+++ мэджик/мэджик/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace мэджик
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            String source = "123456797";
+            for (int i = 0; i < source.Length; i++) 
+            {
+                Console.WriteLine(source[i]);
+            }
+
+    }
+}
